c7e6095d80a1309506b0f9ae83ec49355b429cf2
Theofilos Augoustis <theofilos.augoustis@gmail.com>
2025-08-06 13:30:35 +0200

    Setup basic hugo website repo

---
+++ flake.nix
@@ -0,0 +1,44 @@
+{
+	description = "Personal Website";
+
+	inputs = {
+		nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+
+		utils.url = "github:numtide/flake-utils";
+
+		hugo-poison-theme = {
+			url = "github:lukeorth/poison";
+			flake = false;
+		};
+	};
+
+	outputs = { self, nixpkgs, utils, hugo-poison-theme, ...}:
+	utils.lib.eachDefaultSystem (system:
+	let
+		pkgs = import nixpkgs {
+			inherit system;
+		};
+	in {
+		packages.default = pkgs.stdenv.mkDerivation {
+			name = "taugoust.com";
+			src = self;
+
+			buildPhase = ''
+				mkdir -p themes/poison
+				cp -r ${hugo-poison-theme}/* themes/poison
+				${pkgs.hugo}/bin/hugo
+			'';
+
+			installPhase = "cp -r public $out";
+		};
+
+		apps.default = utils.lib.mkApp {
+			drv = pkgs.hugo;
+		};
+
+		devShells.default = pkgs.mkShell {
+			buildInputs = [ pkgs.hugo ];
+		};
+	});
+}
+
